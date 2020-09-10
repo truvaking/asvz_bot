2a59bca496c9930cc2e9985b83eabf450d6a4357
Julian Stiefel <jstiefel@student.ethz.ch>
2020-09-10 16:37:58 +0200

    wip

--- asvz_bot_copy.py
+++ asvz_bot_copy.py
@@ -1,3 +1,12 @@
 #!/user/bin/env python
 
+import time
+from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
 print("Test")
